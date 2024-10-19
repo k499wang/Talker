18bc918243db203627724f1fac423b032a4de328
Kevin Wang <k499wang@uwaterloo.ca>
2024-10-19 17:25:40 -0400

    Update CORS Policy

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(cors({
 app.use((req, res, next) => {
 
 
-    res.header('Access-Control-Allow-Origin', '*'); // Allow All
+    res.header('Access-Control-Allow-Origin', "*"); // Allow All Origins
     res.header('Access-Control-Allow-Credentials', 'true'); // Allow cookies
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
